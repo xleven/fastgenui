78865606cecc62430c8ad4ccee67b982c723c232
xleven <10850975+xleven@users.noreply.github.com>
2024-05-16 09:47:08 +0800

    add langchain tracer

--- functions/chat.ts
+++ functions/chat.ts
@@ -1,10 +1,18 @@
 import * as hub from "langchain/hub";
+import { Client } from "langsmith";
+import { LangChainTracer } from "langchain/callbacks";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { ChatOpenAI } from "@langchain/openai";
 
 
 export async function onRequestPost(context) {
   try {
+    const tracer = new LangChainTracer(
+      {
+        client: new Client({ apiKey: context.env.LANGCHAIN_API_KEY }),
+        projectName: context.env.LANGCHAIN_PROJECT,
+      }
+    );
     const prompt = await hub.pull<ChatPromptTemplate>("xleven/fastgenui");
     const llm = new ChatOpenAI(
       {
@@ -18,9 +26,14 @@ export async function onRequestPost(context) {
     );
     const body = await context.request.formData();
     const message = body.get("message");
-    const resp = await prompt.pipe(llm).invoke({
-      "message": message
-    });
+    const resp = await prompt.pipe(llm).invoke(
+      {
+        "message": message
+      },
+      {
+        callbacks: [tracer],
+      }
+    );
     const match = resp.content.toString().match(new RegExp("<htmlresponse>(.*?)<\/htmlresponse>", "s"));
     const snippet = match ? match[0] : "";
     const html = `<div class="bg-gray-200 rounded-lg p-3 self-start max-w-[75%]">${snippet}</div>`
